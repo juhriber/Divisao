cc0504697794d7cf2cd86ffa68e8272febc3f5a9
juhriber <j.de.s.r.25@gmail.com>
2021-04-09 22:33:14 -0300

    Finalização do exercício Divisao

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Divisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor1;
+            double valor2;
+            double valor3;
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;  
+            Console.Write("Digite o 1ºValor: ");
+            valor1 = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.DarkGreen;  
+            Console.Write("Digite o 2ºValor: ");
+            valor2 = double.Parse(Console.ReadLine());
+            Console.Write("\n");
+            Console.WriteLine();
+            Console.ResetColor();
+
+            valor3 = valor1 / valor2;
+            
+            if(valor2 > 0)
+            {
+                Console.WriteLine($"{valor1} dividido por {valor2} é = {valor3}");
+                Console.WriteLine();
+                Console.ResetColor();
+
+            }
+            else
+            {
+                Console.ResetColor();  
+                Console.WriteLine("Não é possivel dividir por zero");
+                Console.WriteLine();
+            }
+        }
+    }
+}   
